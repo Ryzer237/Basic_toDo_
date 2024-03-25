86330df57a6ad19828e6c1dfa6a28d39cb8a74e2
Sudhanshu Ranjan <138241338+Ryzer237@users.noreply.github.com>
2024-03-25 12:17:33 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
-mongoose.connect("mongodb+srv://sudhanshuranjan237:ranjan723@cluster0.fui1yi6.mongodb.net/");
-//mongodb+srv://sudhanshuranjan237:ranjan723@cluster0.fui1yi6.mongodb.net/
+mongoose.connect("##);
+
 const todoSchema=mongoose.Schema({
     title:String,
     description:String,
